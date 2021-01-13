dc06e09f71bee3cd91318cd44717f66a2c5b1d3f
JHJ-dev <55976986+JHJ-dev@users.noreply.github.com>
2021-01-14 07:21:39 +0900

    절차적 도시 생성
    도로 생성

---
+++ 절차적 도시 생성/Assets/Scripts/RoadHelper.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoadHelper : MonoBehaviour
+{
+    public GameObject roadStraight, roadCorner, road3way, road4way, roadEnd;
+    private Dictionary<Vector3Int, GameObject> roadDictionary = new Dictionary<Vector3Int, GameObject>();
+    private HashSet<Vector3Int> fixRoadCandidates = new HashSet<Vector3Int>();
+
+    public void PlaceStreetPositions(Vector3 startPosition, Vector3 direction, int length)
+    {
+        var rotation = Quaternion.identity;
+        if (direction.x == 0)
+        {
+            rotation = Quaternion.Euler(0, 90, 0);
+        }
+
+        for (int i = 0; i < length; i++)
+        {
+            var position = Vector3Int.RoundToInt(startPosition + direction * i);
+            if (roadDictionary.ContainsKey(position))
+            {
+                continue;
+            }
+            var road = Instantiate(roadStraight, position, rotation, transform);
+            roadDictionary.Add(position, road);
+            if (i == 0 || i == length - 1)
+            {
+                fixRoadCandidates.Add(position);
+            }
+        }
+    }
+}
---
+++ 절차적 도시 생성/Assets/Scripts/Visualizer.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SVS
+{
+    public class Visualizer : MonoBehaviour
+    {
+        public LSystemGenerator lsystem;
+        List<Vector3> positions = new List<Vector3>();
+
+        public RoadHelper RoadHelper;
+        
+        //public GameObject prefab;
+        //public Material lineMaterial;
+
+        private int length = 8;
+        private float angle = 90;
+
+        public int Length
+        {
+            get
+            {
+                if (length > 0)
+                {
+                    return length;
+                }
+                else
+                {
+                    return 1;
+                }
+            }
+            set => length = value;
+        }
+
+        private void Start()
+        {
+            var sequence = lsystem.GenerateSentence();
+            VisualizeSequence(sequence);
+        }
+
+        private void VisualizeSequence(string sequence)
+        {
+            Stack<AgentParameters> savePoints = new Stack<AgentParameters>();
+            var currentPosition = Vector3.zero;
+
+            Vector3 direction = Vector3.forward;
+            Vector3 tempPosition = Vector3.zero;
+
+            positions.Add(currentPosition);
+
+            foreach (var letter in sequence)
+            {
+                SimpleVisualizer.EncodingLetters encoding = (SimpleVisualizer.EncodingLetters) letter;
+                switch (encoding)
+                {
+                    case SimpleVisualizer.EncodingLetters.save:
+                        savePoints.Push(new AgentParameters
+                        {
+                            position = currentPosition,
+                            direction = direction,
+                            length = Length
+                        });
+                        break;
+                    case SimpleVisualizer.EncodingLetters.load:
+                        if (savePoints.Count > 0)
+                        {
+                            var agentParameter = savePoints.Pop();
+                            currentPosition = agentParameter.position;
+                            direction = agentParameter.direction;
+                            Length = agentParameter.length;
+                        }
+                        else
+                        {
+                            throw new System.Exception("Dont have saved point in our stack");
+                        }
+
+                        break;
+                    case SimpleVisualizer.EncodingLetters.draw:
+                        tempPosition = currentPosition;
+                        currentPosition += direction * length;
+                        RoadHelper.PlaceStreetPositions(tempPosition, Vector3Int.RoundToInt(direction), length);
+                        Length -= 2;
+                        positions.Add(currentPosition);
+                        break;
+                    case SimpleVisualizer.EncodingLetters.turnRight:
+                        direction = Quaternion.AngleAxis(angle, Vector3.up) * direction;
+                        break;
+                    case SimpleVisualizer.EncodingLetters.turnLeft:
+                        direction = Quaternion.AngleAxis(-angle, Vector3.up) * direction;
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+}
