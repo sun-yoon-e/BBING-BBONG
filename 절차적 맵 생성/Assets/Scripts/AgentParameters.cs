990f59e97a2e06abd0bba61e606e2598a958cd23
JHJ-dev <55976986+JHJ-dev@users.noreply.github.com>
2021-02-10 21:12:34 +0900

    ?

---
+++ 절차적 맵 생성/Assets/Scripts/AgentParameters.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AgentParameters
+{
+    public Vector3 position, direction;
+    public int length;
+    
+}
