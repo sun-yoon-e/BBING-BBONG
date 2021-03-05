97eed64ac46d5925922fe25bfa1991c3ce7aed65
uhbbang3 <ansdnjswjd33@gmail.com>
2021-03-05 16:41:10 +0900

    ChangeTerrainHeight 코드 오류 수정

--- 혜진/Road/Assets/Scripts/ChangeTerrainHeight.cs
+++ 혜진/Road/Assets/Scripts/ChangeTerrainHeight.cs
@@ -8,14 +8,14 @@ public class ChangeTerrainHeight : MonoBehaviour
     public Terrain TerrainMain;
 
     public float terrainHeight = 0.4f;
-    
+
     public void FixTerrainHeight(List<Vector3Int> roadPositions)
     {
         Vector3 terrainSize = TerrainMain.terrainData.size;
         int terrainSizeX = (int)terrainSize.x;
-        int terrainSizeZ = (int) terrainSize.z;
-        
-        int roadX; 
+        int terrainSizeZ = (int)terrainSize.z;
+
+        int roadX;
         int roadZ;
 
         foreach (var position in roadPositions)
@@ -26,21 +26,22 @@ public class ChangeTerrainHeight : MonoBehaviour
                 roadX = position.x;
                 roadZ = position.z;
 
-            float[,] heights = TerrainMain.terrainData.GetHeights(0, 0, terrainSizeX, terrainSizeZ);
+                float[,] heights = TerrainMain.terrainData.GetHeights(0, 0, terrainSizeX, terrainSizeZ);
 
-            for (int i = -5; i < 5; i++)
-            {
-                for (int j = -5; j < 5; j++)
+                for (int i = -5; i < 5; i++)
                 {
-                    if (Mathf.Abs(i) == Mathf.Abs(j))
+                    for (int j = -5; j < 5; j++)
                     {
-                        terrainHeight = 0.4f + Mathf.Abs(i) * 0.01f;
+                        if (Mathf.Abs(i) == Mathf.Abs(j))
+                        {
+                            terrainHeight = 0.4f + Mathf.Abs(i) * 0.01f;
+                        }
+                        heights[roadZ + i + terrainSizeX / 2, roadX + j + terrainSizeZ / 2] = terrainHeight;
+                        // 음수여서 오류남
                     }
-                    heights[roadZ + i + terrainSizeX/2, roadX + j + terrainSizeZ/2] = terrainHeight;
-                    // 음수여서 오류남
                 }
+                TerrainMain.terrainData.SetHeights(0, 0, heights);
             }
-            TerrainMain.terrainData.SetHeights(0, 0, heights);
         }
     }
 }
