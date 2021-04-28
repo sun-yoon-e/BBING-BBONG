3ad87d0a2e1445803cce7ba37c051d9c589eab0e
uhbbang3 <ansdnjswjd33@gmail.com>
2021-04-28 17:06:18 +0900

    건물 배치 가능 구역 설정

---
+++ 원정/BSP tree road generator/Assets/Scripts/PlacementBuilding.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlacementBuilding : MonoBehaviour
+{
+    public GameObject buildingPrefab;
+
+    private void Start()
+    {
+        RoadGenerator road = GetComponent<RoadGenerator>();
+
+        for (int i = 0; i < road.vertices.Length; ++i)
+        {
+            if (road.isBuildingPlace[i] == false)
+                continue;
+
+            //if (i % (road.xSize + 1) != 0)
+            {
+                Instantiate(buildingPrefab, road.vertices[i], Quaternion.identity);
+                
+                Vector3 size = buildingPrefab.GetComponent<Renderer>().bounds.size;
+                //i += (int)size.x;
+            }
+        }
+    }
+}
