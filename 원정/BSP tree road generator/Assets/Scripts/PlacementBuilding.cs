25b5c081afae4432d3f92ab36ab70eba4cd742e3
uhbbang3 <ansdnjswjd33@gmail.com>
2021-04-29 17:07:04 +0900

    건물 방향 수정

--- 원정/BSP tree road generator/Assets/Scripts/PlacementBuilding.cs
+++ 원정/BSP tree road generator/Assets/Scripts/PlacementBuilding.cs
@@ -12,26 +12,50 @@ public class PlacementBuilding : MonoBehaviour
 
         for (int i = 0; i < road.vertices.Length; ++i)
         {
-            if (road.isBuildingPlace[i] == false)
+            if (road.isBuildingPlace[i] == (int)buildingDirection.NOTBUILDINGPLACE)
                 continue;
 
             int prefab = Random.Range(1, buildingPrefab.Length);
 
-            Instantiate(buildingPrefab[prefab], road.vertices[i], Quaternion.identity);
+            if(road.isBuildingPlace[i] == (int)buildingDirection.DOWN)
+            {
+                Instantiate(buildingPrefab[prefab], road.vertices[i], Quaternion.identity);
+            }
+            else if(road.isBuildingPlace[i] == (int)buildingDirection.UP)
+            {
+                Instantiate(buildingPrefab[prefab], road.vertices[i], Quaternion.Euler(0, 180, 0));
+            }
+            else if (road.isBuildingPlace[i] == (int)buildingDirection.LEFT)
+            {
+                Instantiate(buildingPrefab[prefab], road.vertices[i], Quaternion.Euler(0, 90, 0));
+            }
+            else if (road.isBuildingPlace[i] == (int)buildingDirection.RIGHT)
+            {
+                Instantiate(buildingPrefab[prefab], road.vertices[i], Quaternion.Euler(0, -90, 0));
+            }
+
 
             Vector3 size = buildingPrefab[prefab].GetComponent<MeshRenderer>().bounds.size;
 
-            if (road.isBuildingPlace[i + 1])
+            if (road.isBuildingPlace[i + 1] != (int)buildingDirection.NOTBUILDINGPLACE)
             {
                 i += ((int)size.x - 1) / 10;
             }
-
-            if (road.isBuildingPlace[i + road.xSize + 1])
+            if (road.isBuildingPlace[i + road.xSize + 1] != (int)buildingDirection.NOTBUILDINGPLACE)
             {
                 for (int j = 0; j < (int)size.x / 5; ++j)
                     if (i + (road.xSize + 1) * j < road.xSize * road.zSize)
-                        road.isBuildingPlace[i + (road.xSize + 1) * j] = false;
+                        road.isBuildingPlace[i + (road.xSize + 1) * j] = (int)buildingDirection.NOTBUILDINGPLACE;
             }
         }
     }
+
+    enum buildingDirection
+    {
+        NOTBUILDINGPLACE,
+        DOWN,
+        UP,
+        RIGHT,
+        LEFT
+    };
 }
