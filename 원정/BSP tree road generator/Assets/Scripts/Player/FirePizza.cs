33091f4f4f55667a7f5b93a9237800d69dbb44a2
JHJ-dev <wjs3662@gmail.com>
2021-05-13 23:17:07 +0900

    플레이어 애니메이션, 미니맵 추가

--- 원정/BSP tree road generator/Assets/Scripts/Player/FirePizza.cs
+++ 원정/BSP tree road generator/Assets/Scripts/Player/FirePizza.cs
@@ -8,23 +8,30 @@ public class FirePizza : MonoBehaviour
     public Transform firePos;
     public Transform targetPos;
     public GameObject pizza;
-
+    
     public float coolTime = 20f;
 
     private float nextTimeToFire = 0f;
+    private Animator animator;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
 
     private void Update()
     {
         if (Input.GetButton("Fire1") && Time.time >= nextTimeToFire)
         {
             nextTimeToFire = Time.time + 20f / coolTime;
-            Fire();
+            animator.SetTrigger("FirePizza");
         }
     }
 
     void Fire()
     {
         GameObject pizzaObject = Instantiate(pizza);
+        pizzaObject.tag = "Player";
         pizzaObject.transform.position = firePos.position;
         var dir = targetPos.position - firePos.position;
         pizzaObject.transform.forward = dir;
---
+++ 원정/BSP tree road generator/Assets/Scripts/gameTimer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net.Mime;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class gameTimer : MonoBehaviour
+{
+    public Text timerText;
+    public bool timerActive = true;
+    public float limitTime = 600;       //제한시간 600초(10분)
+    private float currentTime;
+    
+    
+    void Start()
+    {
+        currentTime = limitTime;
+    }
+    
+    void Update()
+    {
+        if (timerActive == true)
+        {
+            currentTime = currentTime - Time.deltaTime;
+        }
+        TimeSpan time = TimeSpan.FromSeconds(currentTime);
+        timerText.text = time.Minutes.ToString() + ":" + time.Seconds.ToString();
+    }
+}
