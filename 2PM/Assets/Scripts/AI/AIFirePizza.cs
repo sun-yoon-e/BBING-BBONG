091fd215c72167d07bace9a629c763d014572cbd
uhbbang3 <ansdnjswjd33@gmail.com>
2021-08-16 05:05:09 +0900

    AI 추가

---
+++ 2PM/Assets/Scripts/AI/AIFirePizza.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIFirePizza : MonoBehaviour
+{
+    AIMovement movementScript;
+    AIRBController rbController;
+    Rigidbody rb;
+
+    public LayerMask whatIsDoor;
+    Collider[] col;
+
+    public GameObject pizzaPrefab;
+    public Transform firePos;
+    GameObject pizza;
+
+    public float sight;
+
+    void Start()
+    {
+        movementScript = GetComponent<AIMovement>();
+        //rbController = GetComponent<AIRBController>();
+
+        //rb = rbController.GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if(movementScript.isArriveDestination == true)
+        {
+            //print("기다림..");
+            StartCoroutine("InstantiatePizza");
+            StartCoroutine("resetSettings");
+        }
+    }
+
+    IEnumerator InstantiatePizza()
+    {
+        yield return new WaitForSeconds(4f);
+
+        col = Physics.OverlapSphere(transform.position, sight, whatIsDoor);
+
+        if (col[0].enabled)
+        {
+            Vector3 vec = col[0].transform.position - transform.position;
+            vec.Normalize();
+            Quaternion pizzaRotation = Quaternion.LookRotation(vec);
+            Vector3 pizzaPosition = firePos.position;
+
+            pizza = Instantiate(pizzaPrefab, pizzaPosition, pizzaRotation);
+            
+
+            //print(col[0].transform.position);
+            Destroy(col[0]);
+        }
+
+        StopCoroutine("InstantiatePizza");
+    }
+
+    IEnumerator resetSettings()
+    {
+        yield return new WaitForSeconds(3.9f);
+
+        movementScript.SetAIDestination();
+        movementScript.isStopPosition = false;
+        movementScript.isArriveDestination = false;
+
+        StopCoroutine("resetSettings");
+    }
+
+}
---
+++ 2PM/Assets/Scripts/AI/AIMovement.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AIMovement : MonoBehaviour
+{
+    RoadGenerator road;
+    public Rigidbody rb;
+
+    public Vector3 direction;
+
+    GameObject navObject;
+    [HideInInspector] public NavMeshAgent agent;
+
+    [HideInInspector] public float distance;
+
+    public bool isLookAgent = false;
+    public bool isArriveDestination = false;
+
+    Vector3 stopPosition;
+    public bool isStopPosition;
+
+    void Start()
+    {
+        road = GameObject.Find("Road Generator").GetComponent<RoadGenerator>();
+        rb = GetComponent<Rigidbody>();
+
+        CreateNavMeshAgentObject();
+    }
+
+    private void Update()
+    {
+        CalculateDirection();
+        CheckIsSetDestination();
+
+        
+        if (isArriveDestination)
+        {
+            //if (!isStopPosition)
+            //{
+            //    stopPosition = transform.position;
+            //    isStopPosition = true;
+            //}
+            //transform.position = stopPosition;
+
+            rb.constraints = RigidbodyConstraints.FreezeAll;
+        }
+        else
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction), 0.07f);
+    }
+
+    void CreateNavMeshAgentObject()
+    {
+        navObject = new GameObject("AI NavMesh Agent");
+        navObject.transform.position = transform.position;
+        
+        agent = navObject.AddComponent<NavMeshAgent>();
+
+        agent.speed = 20f;
+        agent.radius = 0.1f;
+        agent.acceleration = 50f;
+        agent.avoidancePriority = 1;
+        agent.tag = "AI";
+        agent.autoBraking = true;
+
+        SetAIDestination();
+    }
+
+    public void SetAIDestination()
+    {
+        int destination = Random.Range(1, road.middleRoadNum);
+        agent.SetDestination(road.passibleItemPlace[destination]);
+    }
+
+    void CheckIsSetDestination()
+    {
+        if( agent.transform.position == agent.destination)
+        {
+            isArriveDestination = true;
+        }
+    }
+
+    void CalculateDirection()
+    {
+        direction = agent.transform.position - transform.position;
+        distance = Mathf.Sqrt(direction.x * direction.x + direction.z * direction.z);
+        
+        if (distance > 7)
+            agent.isStopped = true;
+        else
+            agent.isStopped = false;
+    }
+}
