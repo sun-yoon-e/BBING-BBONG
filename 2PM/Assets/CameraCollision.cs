f81754644b1a2687c763cac8eac08106e3755247
JHJ-dev <wjs3662@gmail.com>
2021-08-24 22:55:16 +0900

    카메라 충돌, 그림자

---
+++ 2PM/Assets/CameraCollision.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraCollision : MonoBehaviour
+{
+    public GameObject camera;
+    public float maxDis;
+    public LayerMask layers;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 dir = camera.transform.position - transform.position;
+
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, dir, out hit, maxDis, layers))
+        {
+            float dis = Vector3.Distance(transform.position, hit.point);
+            camera.GetComponent<Gadd420.ThirdPersonCamera>().distance = dis;
+        }
+        else
+        {
+            camera.GetComponent<Gadd420.ThirdPersonCamera>().distance = maxDis;
+        }
+    }
+}
