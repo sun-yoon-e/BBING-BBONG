b0f6a7caf20df015749b5194b107553d1454b812
uhbbang3 <ansdnjswjd33@gmail.com>
2021-09-03 03:15:45 +0900

    Car 연동 수정

--- 2PM/Assets/Scripts/Obstacle/Car.cs
+++ 2PM/Assets/Scripts/Obstacle/Car.cs
@@ -6,12 +6,12 @@ public class Car : MonoBehaviour
 {
     private GameClient gameClient = GameClient.Instance;
 
-    ObstacleGenerator obstacle;
+    //ObstacleGenerator obstacle;
     NavMeshAgent agent;
 
     private void Start()
     {
-        obstacle = GameObject.Find("Obstacle Generator").GetComponent<ObstacleGenerator>();
+        //obstacle = GameObject.Find("Obstacle Generator").GetComponent<ObstacleGenerator>();
         agent = transform.parent.GetComponent<NavMeshAgent>();
     }
 
@@ -20,11 +20,11 @@ public class Car : MonoBehaviour
         transform.localPosition = new Vector3(0, 0, 0);
         transform.localRotation = Quaternion.Euler(transform.eulerAngles.x, 180.0f, transform.eulerAngles.z);
 
-        if (gameClient.isGameStarted && gameClient.client_host)
-        {
-            if (gameObject != null)
-                obstacle.MoveCar(gameObject.transform.parent.gameObject, transform.parent.position, transform.rotation.eulerAngles);
-        }
+        //if (gameClient.isGameStarted && gameClient.client_host)
+        //{
+        //    if (gameObject != null)
+        //        obstacle.MoveCar(gameObject.transform.parent.gameObject, transform.parent.position, transform.rotation.eulerAngles);
+        //}
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -53,9 +53,6 @@ public class Car : MonoBehaviour
             }
 
             Rigidbody targetRB = collision.gameObject.GetComponent<Rigidbody>();
-
-            float cal = collision.relativeVelocity.x + collision.relativeVelocity.y + collision.relativeVelocity.z;
-            //print(cal);
 
             Vector3 inNormal = Vector3.Normalize(transform.position - collision.gameObject.transform.position);
             Vector3 bounceVector = Vector3.Reflect(collision.relativeVelocity, inNormal);
--- 2PM/Assets/Scripts/Obstacle/CarNavmeshAgent.cs
+++ 2PM/Assets/Scripts/Obstacle/CarNavmeshAgent.cs
@@ -3,15 +3,19 @@ using UnityEngine.AI;
 
 public class CarNavmeshAgent: MonoBehaviour
 {
+    private GameClient gameClient = GameClient.Instance;
+
     NavMeshAgent agent;
     RoadGenerator road;
+    ObstacleGenerator obstacle;
 
     int stopTime = 0;
 
     private void Awake()
     {
         road = GameObject.Find("Road Generator").GetComponent<RoadGenerator>();
-        
+        obstacle = GameObject.Find("Obstacle Generator").GetComponent<ObstacleGenerator>();
+
         agent = GetComponent<NavMeshAgent>();
     }
 
@@ -38,6 +42,12 @@ public class CarNavmeshAgent: MonoBehaviour
                 agent.isStopped = false;
                 stopTime = 0;
             }
+        }
+
+        if (gameClient.isGameStarted && gameClient.client_host)
+        {
+            if (gameObject != null)
+                obstacle.MoveCar(gameObject, transform.position, transform.rotation.eulerAngles);
         }
     }
 }
